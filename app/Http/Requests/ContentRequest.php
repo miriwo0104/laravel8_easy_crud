21b0031a2d0106b527f738be3e4d6ce090053c28
shun okawa <shunokawa@gmail.com>
2021-12-06 06:45:09 +0900

    バリデーションチェック前にpostされた値を更新する処理 実装完了

--- app/Http/Requests/ContentRequest.php
+++ app/Http/Requests/ContentRequest.php
@@ -27,4 +27,29 @@ class ContentRequest extends FormRequest
             'content' => ['required', 'num-only'],
         ];
     }
+
+    /**
+     * バリーデーションのためにデータを準備
+     *
+     * @return void
+     */
+    public function prepareForValidation() : void
+    {
+        // 入力された値の末尾にintの9999を付与する
+        $this->replace([
+            'content' => $this->input('content') . 9999,
+        ]);
+        // 入力された値の末尾にintの9999を付与する
+        $this->merge([
+            'name' => 'test',
+        ]);
+        // 入力された値の末尾にintの9999を付与する
+/*         $this->merge([
+            'content' => $this->input('content') . 9999,
+        ]); */
+        // 入力された値をすべてintの0000に置き換える
+/*         $this->replace([
+            'content' => '0000',
+        ]); */
+    }
 }
